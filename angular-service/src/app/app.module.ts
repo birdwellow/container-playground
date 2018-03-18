806166979cd250727cacd40fc7a3a8cf3e729dfd
Florian Vogel <florian.vogel@iteratec.de>
2018-03-18 11:07:36 +0100

    Add bootstrap & ng-bootstrap

--- angular-service/src/app/app.module.ts
+++ angular-service/src/app/app.module.ts
@@ -1,16 +1,17 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
 import { AppRoutingModule } from './app-routing.module';
 
 import { AppComponent } from './app.component';
-
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
+    NgbModule.forRoot(),
     BrowserModule,
     AppRoutingModule
   ],
