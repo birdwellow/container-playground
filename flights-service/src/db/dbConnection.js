669d3c71527be5e970e45fb05474ea5f1c39c495
Florian Vogel <florian.vogel@iteratec.de>
2018-04-17 18:39:28 +0200

    Parameterize DB connection with env variables

--- flights-service/src/db/dbConnection.js
+++ flights-service/src/db/dbConnection.js
@@ -1,5 +1,11 @@
 var mongoose = require('mongoose');
 
-mongoose.connect('mongodb://localhost:27017/flights');
+const HOST = process.env.MONGO_HOST || 'localhost';
+const PORT = process.env.MONGO_PORT || '27017';
+const DATABASE = process.env.MONGO_DATABASE || 'flights';
+
+var connectString = `mongodb://${HOST}:${PORT}/${DATABASE}`;
+
+mongoose.connect(connectString);
 
 module.exports = mongoose;
